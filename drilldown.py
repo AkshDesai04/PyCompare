d42e6a55e6ee5238a49e1c4594992285421330c9
poneoneo <onealzero@gmail.com>
2024-05-07 23:11:18 -0400

    replace os by pathlib to improve file and directories management

--- drilldown.py
+++ drilldown.py
@@ -1,14 +1,16 @@
 import os
 
-def drilldown(folder_path, extensions = {'.bmp', '.dib', '.jpeg', '.jpg', '.jp2', '.png', '.pbm', '.pgm', '.ppm', '.sr', '.ras', '.tiff', '.tif', '.exr', '.jxr', '.pfm', '.pds', '.pfm', '.viff', '.xbm', '.xpm', '.dds', '.eis', '.mng', '.web', '.hei', '.hei', '.av'}):
-    image_files = []
-    for root, dirs, files in os.walk(folder_path):
-        for file in files:
-            file_extension = os.path.splitext(file)[1].lower()
-            if file_extension in extensions:
-                image_files.append(os.path.join(root, file))
-    return image_files
+from pathlib import Path
 
+def drilldown(folder_path, extensions={'.bmp', '.dib', '.jpeg', '.jpg', '.jp2', '.png', '.pbm', '.pgm', '.ppm', '.sr', '.ras', '.tiff', '.tif', '.exr', '.jxr', '.pfm', '.pds', '.pfm', '.viff', '.xbm', '.xpm', '.dds', '.eis', '.mng', '.web', '.hei', '.hei', '.av'}):
+    image_files = []
+    folder_path = Path(folder_path)
+    
+    for file_path in folder_path.glob('**/*'):
+        if file_path.is_file() and file_path.suffix.lower() in extensions:
+            image_files.append(file_path)
+    
+    return image_files
 
 folder = input('Provide the folder path: ') # Getting the path of the folder to drill down into
 images = drilldown(folder) # Getting the list of images in the folder and all sub-folders
