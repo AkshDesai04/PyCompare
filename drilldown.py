b6e949dd39f1e8a575dbd2bddd8b9c708830d099
Aksh Desai <aksh.d.4002@gmail.com>
2024-05-08 13:22:25 +0530

    removed testing code from file

--- drilldown.py
+++ drilldown.py
@@ -11,7 +11,3 @@ def drilldown(folder_path, extensions={'.bmp', '.dib', '.jpeg', '.jpg', '.jp2', 
             image_files.append(file_path)
     
     return image_files
-
-folder = input('Provide the folder path: ') # Getting the path of the folder to drill down into
-images = drilldown(folder) # Getting the list of images in the folder and all sub-folders
-print(images)
