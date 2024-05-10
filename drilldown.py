16e7f59c977fe726e3680229f9b85a4d0b653912
Madhav Gohel <madhavgohel2@gmail.com>
2024-05-10 20:26:04 +0530

    Refactor drilldown function signature

--- drilldown.py
+++ drilldown.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-def drilldown(targeted_directory:str, extensions:list[str] = ['.bmp', '.dib', '.jpeg', '.jpg', '.jp2', '.png', '.pbm', '.pgm', '.ppm', '.sr', '.ras', '.tiff', '.tif', '.exr', '.jxr', '.pfm', '.pds', '.pfm', '.viff', '.xbm', '.xpm', '.dds', '.eis', '.mng', '.web', '.hei', '.hei', '.av']):
+def drilldown(targeted_directory, extensions = ['.bmp', '.dib', '.jpeg', '.jpg', '.jp2', '.png', '.pbm', '.pgm', '.ppm', '.sr', '.ras', '.tiff', '.tif', '.exr', '.jxr', '.pfm', '.pds', '.pfm', '.viff', '.xbm', '.xpm', '.dds', '.eis', '.mng', '.web', '.hei', '.hei', '.av']):
     image_files:list[str]= []
     folder_path:Path = Path(targeted_directory)
     
